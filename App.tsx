1f1cc61420d5131488841acf5fd2a0a0b8a40e8f
Louis Raetz <louis@fintory.com>
2020-01-14 11:27:58 +0100

    added splashscreen/image prefetching in root tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,58 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { AppLoading, SplashScreen } from 'expo'
+import { Asset } from 'expo-asset'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  )
+export default class App extends React.Component {
+  state = {
+    isSplashReady: false,
+    isAppReady: false,
+  }
+
+  render() {
+    if (!this.state.isSplashReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheSplashResourcesAsync}
+          onFinish={() => this.setState({ isSplashReady: true })}
+          onError={console.warn}
+          autoHideSplash={false}
+        />
+      )
+    }
+
+    if (!this.state.isAppReady) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Image source={require('./src/assets/splash.png')} onLoad={this._cacheResourcesAsync} />
+        </View>
+      )
+    }
+
+    return (
+      <View style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text>@typescript/expo-starter</Text>
+      </View>
+    )
+  }
+
+  _cacheSplashResourcesAsync = async () => {
+    const gif = require('./src/assets/splash.png')
+    return Asset.fromModule(gif).downloadAsync()
+  }
+
+  _cacheResourcesAsync = async () => {
+    SplashScreen.hide()
+    const images = [
+      // Insert prefetching shit here
+      // require('EXAMPLE IMAGE PATH'),
+    ]
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync()
+    })
+
+    await Promise.all(cacheImages)
+    this.setState({ isAppReady: true })
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
